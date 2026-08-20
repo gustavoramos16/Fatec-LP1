2ed366906ef29792fcd3f9fe236769b6fb1534f7
gustavoramos16 <gu07ramos@gmail.com>
2026-08-19 21:13:15 -0300

    feat: add menu

--- src/Main.java
+++ src/Main.java
@@ -6,36 +6,56 @@ import exercicio5.Camelos;
 import exercicio6.Viagem;
 import exercicio7.Aluguel;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//        Multiplo multiplo = new Multiplo();
-//        multiplo.receberNumeros();
-//        multiplo.calcularMultiplo();
 
-//        login login = new login();
-//        login.compararSenhas();
+        String opcaoMenu;
 
-//        Mercado mercado = new Mercado();
-//        mercado.definirPreço();
-//        mercado.mostrarResultado();
+        System.out.println("Seja bem vindo ao programa!");
+        System.out.println("1-Verificação de Múltiplo\n" +
+                "2-Sistema de Login Simplificado\n" +
+                "3-Compra com desconto\n" +
+                "4-Temperatura\n" +
+                "5-Os trinta e cinco camelos\n" +
+                "6-Calculadora de viagem\n" +
+                "7-Aluguel de carro");
+        System.out.println("Escolha uma opção do menu");
 
-//        Temperatura temperatura = new Temperatura();
-//        temperatura.receberTemperaturas();
-//        temperatura.classificarCadaUma();
-//        temperatura.mostrarResultado();
+        Scanner scan = new Scanner(System.in);
+        opcaoMenu = scan.next();
 
-//        Camelos camelos = new Camelos();
-//        camelos.receberDados();
-//        camelos.mostrarResultado();
-
-//        Viagem viagem = new Viagem();
-//        viagem.receberDados();
-//        viagem.mostrarResultado();
-
-        Aluguel aluguel = new Aluguel();
-        aluguel.receberDados();
-        aluguel.mostrarResultado();
+        switch(opcaoMenu){
+            case "1": Multiplo multiplo = new Multiplo();
+                multiplo.receberNumeros();
+                multiplo.calcularMultiplo();
+                return;
+            case "2": login login = new login();
+                login.compararSenhas();
+                return;
+            case "3": Mercado mercado = new Mercado();
+                mercado.definirPreço();
+                mercado.mostrarResultado();
+                return;
+            case "4": Temperatura temperatura = new Temperatura();
+                temperatura.receberTemperaturas();
+                temperatura.classificarCadaUma();
+                temperatura.mostrarResultado();
+                return;
+            case "5":  Camelos camelos = new Camelos();
+                camelos.receberDados();
+                camelos.mostrarResultado();
+                return;
+            case "6": Viagem viagem = new Viagem();
+                viagem.receberDados();
+                viagem.mostrarResultado();
+                return;
+            case "7": Aluguel aluguel = new Aluguel();
+                aluguel.receberDados();
+                aluguel.mostrarResultado();
+        }
     }
 }
